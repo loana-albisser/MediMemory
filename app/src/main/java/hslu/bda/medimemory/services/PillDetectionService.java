6169823e37ad84e9bccf8b83c1b1228e84998ce3
tbeugste <andreas.eugster@stud.hslu.ch>
2016-05-31 14:11:50 +0200

    changed property in constructor

--- app/src/main/java/hslu/bda/medimemory/services/PillDetectionService.java
+++ app/src/main/java/hslu/bda/medimemory/services/PillDetectionService.java
@@ -1,7 +1,16 @@
 package hslu.bda.medimemory.services;
 
+import hslu.bda.medimemory.entity.Consumed;
+
 /**
  * Created by manager on 28.04.2016.
  */
 public class PillDetectionService {
+
+    private PillDetectionService(){}
+
+    public static long setConsumed(Consumed consumed){
+        //TODO
+        return  -1;
+    }
 }
